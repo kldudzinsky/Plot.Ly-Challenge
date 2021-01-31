9f030b35fd4a65ce56b31b5518b3e117c1c28059
KLDudzinsky <kldudzinsky@gmail.com>
2021-01-31 15:37:45 -0800

    added files

---
+++ static/js/app.js
@@ -0,0 +1,9 @@
+//use d3.json() to fetch data from JSON file
+d3.json("samples.json").then((incomingData) => {
+    var data = incomingData;
+    //call samples
+    var samples = data.samples;
+
+    //
+    
+});
