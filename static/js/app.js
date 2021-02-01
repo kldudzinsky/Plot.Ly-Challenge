2b7f3774de9967cabf04afe496c8dd5cfe89f5dd
KLDudzinsky <kldudzinsky@gmail.com>
2021-01-31 23:27:01 -0800

    continued work

--- static/js/app.js
+++ static/js/app.js
@@ -4,30 +4,44 @@ d3.json("samples.json").then((incomingData) => {
     //call samples
     var samples = data.samples;
     //console.log(samples)
-//create variable for otu_ids for individual 940
-    var otu_ids=samples.map(function(sample){
-        return sample.otu_ids[0]
-    })
-    //console.log(otu_ids)
-//create variable for sample values for individual 940
 
-    var sample_values =samples.map(function(sample){
-        return sample.sample_values[0]
-    })
-    //console.log(sample_values)
- //create variable for otu lables for individual 940
+//use for each to go through each sample.id and pull
+//top 10 otu_ids, sample_values and otu_lables/
+//then put into empty bar chart 
+    samples.forEach(function(sample){
+        d3.selectAll("body").on("change", dropdown)
+        // //set up  dropdown functions
+        // function dropdown(){
+        //     var CHART=d3.selectAll("#plot").node();
+        //     var menu=d3.select("selDataset");
+        //     var menu_variable=menu.node().value;
+        //     var x=[];
+        //     var y=[];
+        };
+        var otu_ids= samples.map(function(sample){
+            return sample.otu_ids
+        });
+        var slice_otu_ids=otu_ids.slice(0,10);  
+       
+        console.log(slice_otu_ids);
+    //   var trace0= {
+    //       x:[sample.otu_id.slice(0,10)],
+    //       y:[sample.sample_values.slice(0,10)],
+    //       type: "bar",
+    //       mode: 'markers',
+    //       text: sample.otu_labels.slide(0,10)
+    //   };
+    //   var data =[trace0];
+    //   var layout ={
+    //       title: "OTU samples",
+    //       xaxis: {title:"OTU IDs"},
+    //       yaxis: {title:"Sample Value"}
+    //   };
+    //   Plotly.newPlot("plot",data, layout);
 
-   var otu_lables = samples.map(function(sample){
-        return sample.otu_labels[0]
-   })
-    //console.log(otu_lables)
-//use slice for first 10 values for ids, samples and labels for ind 940
-    var top10_otu_ids=otu_ids.slice(0,10);
-    var top10_sample_values=sample_values.slice(0,10);
-    var top_10_otu_lables=otu_lables.slice(0,10);
-    console.log(top10_otu_ids);
-    console.log(top10_sample_values);
-    console.log(top_10_otu_lables);
+    });
+    
 
+ 
 
 });
